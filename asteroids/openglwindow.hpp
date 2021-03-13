4dbaca1b9ba3f696932837d47c59d6055a7c1877
gaprilus <78174910+gaprilus@users.noreply.github.com>
2021-03-13 15:32:50 -0300

    Add files via upload

---
+++ asteroids/openglwindow.hpp
@@ -0,0 +1,61 @@
+#ifndef OPENGLWINDOW_HPP_
+#define OPENGLWINDOW_HPP_
+
+#include <imgui.h>
+
+#include <random>
+
+#include "abcg.hpp"
+#include "asteroids.hpp"
+#include "bullets.hpp"
+#include "ship.hpp"
+#include "starlayers.hpp"
+#include "user.hpp"
+
+class user;
+
+class OpenGLWindow : public abcg::OpenGLWindow {
+ protected:
+  void handleEvent(SDL_Event& event) override;
+  void initializeGL() override;
+  void paintGL() override;
+  void paintUI() override;
+  void resizeGL(int width, int height) override;
+  void terminateGL() override;
+
+ private:
+  GLuint m_starsProgram{};
+  GLuint m_objectsProgram{};
+
+  int m_viewportWidth{};
+  int m_viewportHeight{};
+
+  GameData m_gameData;
+
+  Asteroids m_asteroids;
+  Bullets m_bullets;
+  Ship m_ship;
+  StarLayers m_starLayers;
+  
+  abcg::ElapsedTimer m_restartWaitTimer;
+
+  ImFont* m_font{};
+  ImFont* m_font2{};
+
+  std::default_random_engine m_randomEngine;
+
+  void checkCollisions();
+  void checkWinCondition();
+
+  void restart();
+  void update();
+
+  int fase_atual;
+
+  public:
+  user usuario;
+
+
+};
+#endif
+
